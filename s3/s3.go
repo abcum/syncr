46d8dc9f9fc2d6fc3bf88e371067d376a4f901e3
Tobie Morgan Hitchcock <tobie@abcum.com>
2017-06-13 23:12:28 +0100

    Implement Amazon S3 backend support

--- s3/s3.go
+++ s3/s3.go
@@ -15,9 +15,25 @@
 package s3
 
 import (
+	"bytes"
+	"context"
 	"errors"
+	"io"
+	"path"
+	"strings"
 	"sync"
+	"time"
+
+	"github.com/abcum/bump"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
+
+var temp int
 
 var (
 	// ErrSeekPositionNotSupported occurs when a syncable type receives a
@@ -36,25 +52,68 @@ type Options struct {
 	// each write using the buffered data. MinSize should be specified in
 	// megabytes.
 	MinSize int
-	// BufferWrites can be used to determine whether writes are buffered
-	// up to a certain number of bytes, before being synced. This is only
-	// available on syncable types which do not support appendable writes.
-	// BufferWrites should be specified in megabytes.
-	BufferWrites int
 }
 
 // Storage represents a Amazon S3 reader and writer.
 type Storage struct {
 	sync.Mutex
 	opts *Options
+	lock sync.Mutex
+	size struct {
+		min int
+		now int
+	}
+	serv struct {
+		srv *s3.S3
+		ctx context.Context
+	}
+	buff struct {
+		bit []byte
+		wtr *bump.Writer
+		rdr io.ReadCloser
+	}
+	file struct {
+		buck string
+		last string
+		full string
+		path string
+		base string
+		name string
+		extn string
+		main string
+		poss []string
+	}
 }
 
 // New creates a new Syncable storage instance for reading and writing.
-func New(path string, opts *Options) (s *Storage, e error) {
+func New(name string, opts *Options) (s *Storage, e error) {
+
+	p := strings.Index(name, "/")
 
 	s = &Storage{}
 
 	s.opts = opts
+	s.file.buck = name[:p]
+	s.file.full = name[p+1:]
+	s.file.path = path.Dir(s.file.full)
+	s.file.base = path.Base(s.file.full)
+	s.file.extn = path.Ext(s.file.full)
+	s.file.name = s.file.base[:len(s.file.extn)+1]
+	s.file.main = path.Join(s.file.path, s.file.base)
+
+	s.serv.ctx = context.Background()
+
+	s.buff.wtr = bump.NewWriterBytes(&s.buff.bit)
+
+	s.size.min = opts.MinSize * 1024 * 1024
+
+	d := endpoints.UsWest1RegionID
+
+	n := session.Must(session.NewSession())
+
+	r, _ := s3manager.GetBucketRegion(s.serv.ctx, n, s.file.buck, d)
+
+	s.serv.srv = s3.New(n, &aws.Config{Region: aws.String(r)})
 
 	return
 
@@ -63,7 +122,27 @@ func New(path string, opts *Options) (s *Storage, e error) {
 // Close closes the underlying Syncable storage data stream, and prevents
 // any further reads of writes to the Storage instance.
 func (s *Storage) Close() error {
-	return nil
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	defer func() {
+		s.opts = nil
+		s.buff.bit = nil
+		s.buff.wtr = nil
+		s.buff.rdr = nil
+		s.file.last = ""
+		s.file.full = ""
+		s.file.path = ""
+		s.file.base = ""
+		s.file.name = ""
+		s.file.extn = ""
+		s.file.main = ""
+		s.file.poss = nil
+	}()
+
+	return s.stop()
+
 }
 
 // Read reads up to len(p) bytes into p, reading from the Storage data
@@ -72,7 +151,24 @@ func (s *Storage) Close() error {
 // any error encountered. If the Storage has reached the final log file,
 // then an EOF error will be returned.
 func (s *Storage) Read(p []byte) (int, error) {
-	return 0, nil
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	// No data file is open yet so open one.
+	if s.buff.rdr == nil {
+		if err := s.head(); err != nil {
+			return 0, err
+		}
+	}
+
+	n, err := s.buff.rdr.Read(p)
+	if err == io.EOF {
+		err = s.next()
+	}
+
+	return n, err
+
 }
 
 // Write writes len(p) bytes from p to the underlying Storage data
@@ -81,17 +177,47 @@ func (s *Storage) Read(p []byte) (int, error) {
 // will always append data to the end of the Storage data stream, ensuring
 // data is append-only and never overwritten.
 func (s *Storage) Write(p []byte) (int, error) {
-	return 0, nil
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if s.size.min < s.size.now+len(p) {
+		if err := s.sync(); err != nil {
+			return 0, err
+		}
+	}
+
+	s.size.now += len(p)
+
+	return len(p), s.buff.wtr.WriteBytes(p)
+
 }
 
 // Seek sets the offset for the next Read or Write to offset, interpreted
 // according to whence: SeekStart means relative to the start of the file,
-// SeekCurrent means relative to the current offset, and SeekEnd means
+// SeekCurrent means relative to the current offset, and SeekE8nd means
 // relative to the end. Seek returns the new offset relative to the start
 // of the file and an error, if any. In some storage types, Seek only
 // supports seeking to the beginning and end of the data stream.
 func (s *Storage) Seek(offset int64, whence int) (int64, error) {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	if offset != 0 {
+		return 0, ErrSeekPositionNotSupported
+	}
+
+	if whence != 0 && whence != 2 {
+		return 0, ErrSeekPositionNotSupported
+	}
+
+	if whence == 0 {
+		s.head()
+	}
+
 	return 0, nil
+
 }
 
 // Sync ensures that any buffered data in the stream is committed to stable
@@ -99,5 +225,139 @@ func (s *Storage) Seek(offset int64, whence int) (int64, error) {
 // and persisted immediately when a Write is made. On Syncable types which
 // support BufferWrites, then Sync will ensure the data stored is flushed.
 func (s *Storage) Sync() error {
+
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	return s.sync()
+
+}
+
+// ---------------------------------------------------------------------------
+
+func (s *Storage) load(file string) (err error) {
+
+	obj := &s3.GetObjectInput{
+		Bucket: aws.String(s.file.buck),
+		Key:    aws.String(file),
+	}
+
+	out, err := s.serv.srv.GetObject(obj)
+	if err != nil {
+		return err
+	}
+
+	s.buff.rdr = out.Body
+
+	s.file.last = file
+
 	return nil
+
+}
+
+func (s *Storage) sync() (err error) {
+
+	obj := &s3.PutObjectInput{
+		Bucket: aws.String(s.file.buck),
+		Key:    aws.String(s.name()),
+		Body:   bytes.NewReader(s.buff.bit),
+	}
+
+	if _, err = s.serv.srv.PutObject(obj); err != nil {
+		return err
+	}
+
+	if err = s.buff.wtr.ResetBytes(&s.buff.bit); err != nil {
+		return err
+	}
+
+	s.size.now = 0
+
+	return nil
+
+}
+
+func (s *Storage) stop() (err error) {
+
+	if s.buff.rdr == nil {
+		return nil
+	}
+
+	defer func() {
+		s.buff.rdr = nil
+	}()
+
+	return s.buff.rdr.Close()
+
+}
+
+func (s *Storage) head() (err error) {
+
+	err = s.stop()
+	if err != nil {
+		return err
+	}
+
+	for _, file := range s.look() {
+		return s.load(file)
+	}
+
+	return io.EOF
+
+}
+
+func (s *Storage) next() (err error) {
+
+	err = s.stop()
+	if err != nil {
+		return err
+	}
+
+	for _, file := range s.look() {
+		if file > s.file.last {
+			return s.load(file)
+		}
+	}
+
+	return io.EOF
+
+}
+
+func (s *Storage) look() []string {
+
+	if s.file.poss != nil {
+		return s.file.poss
+	}
+
+	obj := &s3.ListObjectsInput{
+		Bucket: aws.String(s.file.buck),
+		Prefix: aws.String(s.file.path),
+	}
+
+	dir, err := s.serv.srv.ListObjects(obj)
+	if err != nil {
+		return nil
+	}
+
+	for _, info := range dir.Contents {
+
+		// Ignore folders and invisibles
+		if (*info.Key)[:1] == "." {
+			continue
+		}
+
+		s.file.poss = append(s.file.poss, *info.Key)
+
+	}
+
+	return s.file.poss
+
+}
+
+func (s *Storage) name() string {
+	return path.Join(s.file.path, s.file.name+"-"+s.time()+s.file.extn)
+}
+
+func (s *Storage) time() string {
+	return time.Now().UTC().Format("2006-01-02T15-04-05.999999999")
 }
